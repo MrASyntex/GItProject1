8d98d7662475d735225150e656ef794dfca73656
MrASyntex <continentaldispatch@gmail.com>
2022-05-14 09:03:38 -0700

    2nd comit

--- src/GitHub/ClassGit1.java
+++ src/GitHub/ClassGit1.java
@@ -3,6 +3,9 @@ package GitHub;
 public class ClassGit1 {
     public static void main(String[] args) {
         System.out.println("Git class");
+        System.out.println("LEts practice ");
+        System.out.println("blah blah blah");
+        
 
     }
 }
