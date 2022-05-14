d7ce75d3fdadf620fa5546486ea019d8de96d071
MrASyntex <continentaldispatch@gmail.com>
2022-05-14 09:46:26 -0700

    4th comit

--- src/GitHub/ClassGit1.java
+++ src/GitHub/ClassGit1.java
@@ -1,11 +1,15 @@
 package GitHub;
 
 public class ClassGit1 {
-    public static void main(String[] args) {
+    public static <let> void main(String[] args) {
         System.out.println("Git class");
         System.out.println("LEts practice ");
         System.out.println("blah blah blah");
         System.out.println("-----------=-=-=-=-=-==-=-=-==-==");
+        String let;
+        System.out.println("875487487548754L");
+
+
 
 
     }
