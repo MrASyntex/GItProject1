cf7c685d3417fb4d2becd103e6952260b2b9a9ca
MrASyntex <continentaldispatch@gmail.com>
2022-05-14 11:21:46 -0700

    5th comit

--- src/GitHub/ClassGit1.java
+++ src/GitHub/ClassGit1.java
@@ -8,7 +8,12 @@ public class ClassGit1 {
         System.out.println("-----------=-=-=-=-=-==-=-=-==-==");
         String let;
         System.out.println("875487487548754L");
+        System.out.println(" im am bored of this particular class");
+        System.out.println("yaaaaaasssssss");
 
+        System.out.println("=-=-----------");
+        System.out.println("83475634987563");
+        
 
 
 
